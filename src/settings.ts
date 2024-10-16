e96f13e8c9aa328402fa96ce26f738eb78762f6a
KateW0912 <wangkeqian0912@gmail.com>
2024-10-15 22:32:28 -0700

    number in footnote

--- src/settings.ts
+++ src/settings.ts
@@ -22,6 +22,6 @@ import {
     hoverDelay: 250,
     numberResetSelector: '',
     scope: '',
-    contentTemplate: `<aside class="littlefoot__popover" id="fncontent:<% id %>"><div class="${CLASS_WRAPPER}"><div class="${CLASS_CONTENT}"><% content %></div></div><div class="${CLASS_TOOLTIP}"></div></aside>`,
+    contentTemplate: `<aside class="littlefoot__popover" id="fncontent:<% id %>"><div class="${CLASS_WRAPPER}"><div class="${CLASS_CONTENT}"><% number %>.<% content %></div></div><div class="${CLASS_TOOLTIP}"></div></aside>`,
     buttonTemplate: `<button class="littlefoot__button" id="<% reference %>" title="See Footnote <% number %>"><svg role="img" aria-labelledby="title-<% reference %>" viewbox="0 0 31 6" preserveAspectRatio="xMidYMid"><title id="title-<% reference %>">Footnote <% number %></title><circle r="3" cx="3" cy="3" fill="white"></circle><circle r="3" cx="15" cy="3" fill="white"></circle><circle r="3" cx="27" cy="3" fill="white"></circle></svg></button>`,
   }
